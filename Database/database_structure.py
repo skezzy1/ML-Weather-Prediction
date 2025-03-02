081a0657f58b30c85cdba4300f5d98ef6118b39e
Serhii Herasymchuk <serhiy1204@gmail.com>
2025-03-02 15:56:15 +0200

    Update structure for actual weather situation and for the future analisis

--- Database/database_structure.py
+++ Database/database_structure.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, ForeignKey, String, Integer, Column, Date, Float, Boolean
+from sqlalchemy import create_engine, ForeignKey, String, Integer, Column, Date, Float, Boolean, DateTime
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 import uuid
@@ -12,22 +12,40 @@ class WeatherPrediction(Base):
     __tablename__ = "WeatherPrediction"
     RedcordID = Column(Integer, primary_key=True, autoincrement=True)
     Location = Column(String)
-    Date = Column(Date)
+    DateTime = Column(DateTime, default=datetime.utcnow))
     Temperature_C = Column(Float)
     Humidity_pct = Column(Float)
     Precipitation_mm = Column(Float)
     Wind_Speed_kmh = Column(Float)
     will_rain = Column(Boolean)
 
-    def __init__(self, RedcordID, Location, Date, Temperature_C, Humidity_pct, Wind_Speed_kmh):
+    def __init__(self, RedcordID, Location, DateTime, Temperature_C, Humidity_pct, Wind_Speed_kmh):
         self.RedcordID = RedcordID
         self.Location = Location
-        self.Date = Date
+        self.DateTime = DateTime
         self.Temperature_C = Temperature_C
         self.Humidity_pct = Humidity_pct
         self.Precipitation_mm = Precipitation_mm
         self.Wind_Speed_kmh = Wind_Speed_kmh
 
+class WeatherActual(Base):
+    __tablename__ = "ActualWeather"
+    RedcordID = Column(Integer, primary_key=True, autoincrement=True)
+    DateTime = Column(DateTime, default=datetime.utcnow))
+    temperature_actual = Column(Float)
+    humidity_actual = Column(Float)
+    wind_speed_actual = Column(Float)
+    precipitation_actual = Column(Float)
+    will_rain_actual = Column(Boolean)
+
+    def __init__(self, RedcordID, DateTime, temperature_actual, humidity_actual, wind_speed_actual, precipitation_actual, will_rain_actual):
+        self.RedcordID = RedcordID
+        self.DateTime = DateTime
+        self.temperature_actual = temperature_actual
+        self.humidity_actual = humidity_actual
+        self.wind_speed_actual = wind_speed_actual
+        self.precipitation_actual = precipitation_actual
+        self.will_rain_actual = will_rain_actual
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
