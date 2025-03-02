d978e7a89a043878fcffdedb202552018b609e66
Serhii Herasymchuk <serhiy1204@gmail.com>
2025-03-02 17:45:51 +0200

    New function for deeper analysis

--- data_preparation/csv_analysis.py
+++ data_preparation/csv_analysis.py
@@ -7,6 +7,14 @@ X = dataset.iloc[: , :].values
 y = dataset.iloc[: , 0].values
 
 #print(X)
-unique_data_meaning = set(y)
+unique_data_meaning = set(y) # Unique cities in dataset
 print(unique_data_meaning)
-print(X)
+print("==================================================================")
+# Check for missing values
+print(dataset.isnull().sum())
+print("==================================================================")
+# Check percentage of missing values per column
+print(dataset.isnull().mean() * 100)
+print("==================================================================")
+duplicates = dataset.duplicated().sum()
+print(f"Number of duplicate rows: {duplicates}")
