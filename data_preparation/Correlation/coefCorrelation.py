82d45230d7b951a745b173dd7d5034fd0240f331
Serhii Herasymchuk <serhiy1204@gmail.com>
2025-04-20 12:36:21 +0300

    Add matrix of correlation

--- data_preparation/Correlation/coefCorrelation.py
+++ data_preparation/Correlation/coefCorrelation.py
@@ -2,8 +2,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
 #df = pd.read_csv("weather_data.csv")
-df = pd.read_csv("data_preparation/csv/cities/lviv_data.csv")
+df = pd.read_csv("data_preparation/csv/data.csv")
 #df = pd.read_csv("data_preparation/austin_weather.csv")
 df.columns = [col.strip() for col in df.columns]
 
@@ -15,8 +16,8 @@ selected_columns = [
     "Precipitation_mm"
 ]
 '''
-city_name = "Львів"
-city_data = df[df['City'] == city_name]
+#city_name = "Львів"
+#city_data = df[df['City'] == city_name]
 selected_columns = [
     "Average Temperature (celsius)",
     "Max Temperature (celsius)",
@@ -54,13 +55,13 @@ df_selected = df[selected_columns]
 
 correlation_matrix = df_selected.corr()
 
-plt.figure(figsize=(17, 15))
+plt.figure(figsize=(10, 8))
 sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap='coolwarm', center=0)
-plt.title(f'Кореляційна матриця погодних параметрів для {city_name}')
+plt.title(f'Кореляційна матриця погодних параметрів')
 plt.show()
 
 print(correlation_matrix)
-city_data.to_csv('data_preparation/csv/cities/lviv_data.csv', index=False, encoding='utf-8')
-correltion_result = f"data_preparation/Correlation/correlation_result_{city_name}.txt"
+#city_data.to_csv('data_preparation/csv/cities/lviv_data.csv', index=False, encoding='utf-8')
+correltion_result = f"data_preparation/Correlation/correlation_result.txt"
 with open(correltion_result,  "w", encoding="utf-8") as file:
     file.write(correlation_matrix.to_string())
